78ac8374336b72a6ce875297b86f7d29d04f2b30
hrytsiukpavlo <overdrive800392@gmail.com>
2022-09-11 23:21:02 +0300

    added basic markup and components
    added basic markup and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 className="content__title">All pizzas</h2>
+					<div className="content__items">
+						<PizzaBlock title="Mexican" price="10.99" />
+						<PizzaBlock />
+						<PizzaBlock />
+						<PizzaBlock />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Categories() {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">All</li>
+				<li>Meat</li>
+				<li>Vegeterian</li>
+				<li>Grill</li>
+				<li>Hot</li>
+				<li>Closed</li>
+			</ul>
+		</div>
+	);
+}
