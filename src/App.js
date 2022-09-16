e8e5536465b2b807ad7d94f2cca1b8c33b030596
hrytsiukpavlo <overdrive800392@gmail.com>
2022-09-16 17:46:29 +0300

    added redux toolkit
    added redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
 import "./App.css";
 import "./scss/app.scss";
 import Header from "./components/Header";
@@ -7,21 +9,37 @@ import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
 import { Route, Routes } from "react-router-dom";
 
+export const SearchContext = createContext();
+
 function App() {
 	const [searchValue, setSearchValue] = useState("");
+	const count = useSelector((state) => state.counter.count);
+	const dispatch = useDispatch();
 
-	console.log(searchValue);
 	return (
-		<div className="wrapper">
-			<Header searchValue={searchValue} setSearchValue={setSearchValue} />
-			<div className="content">
-				<Routes>
-					<Route path="/" element={<Home searchValue={searchValue} />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+		<div>
+			<div>
+				<button aria-label="Increment value" onClick={() => dispatch(increment())}>
+					Increment
+				</button>
+				<span>{count}</span>
+				<button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+					Decrement
+				</button>
 			</div>
 		</div>
+		// <div className="wrapper">
+		// 	<SearchContext.Provider value={{ searchValue, setSearchValue }}>
+		// 		<Header />
+		// 		<div className="content">
+		// 			<Routes>
+		// 				<Route path="/" element={<Home />} />
+		// 				<Route path="/cart" element={<Cart />} />
+		// 				<Route path="*" element={<NotFound />} />
+		// 			</Routes>
+		// 		</div>
+		// 	</SearchContext.Provider>
+		// </div>
 	);
 }
 
